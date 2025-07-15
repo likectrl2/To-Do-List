36a4a02cb5528e4c51004a15fe7f5dde80ea13d4
likectrl2 <likectrl2@gmail.com>
2025-07-15 23:09:18 +0800

    feat(App): wrap with TaskProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { TaskProvider } from './contexts/taskContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TaskProvider>
+      <App />
+    </TaskProvider>
   </StrictMode>,
 )
