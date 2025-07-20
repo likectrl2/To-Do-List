1e68e044cf5f538e10fb0bf00646ac053f6c4c98
likectrl2 <likectrl2@gmail.com>
2025-07-20 13:46:59 +0800

    feat(type): setup some types for using

---
+++ src/types/index.tsx
@@ -0,0 +1,47 @@
+export interface WorkItem {
+    id: string;
+    title: string;
+    description: string;
+    createdAt: number;
+    
+    status: 'toDo' | 'inProgress' | 'completed' | 'canceled';
+    timeFlexibility: 'flexible' | 'semi-flexible' | 'fixed';
+    startTime: number | null;
+    endTime: number | null;
+    deadline: number | null
+    estimateDurationMinutes: number | null;
+
+    importance: number;
+    urgency: number;
+    tags: string[];
+}
+
+export interface Task extends WorkItem {
+    type: 'Task';
+    
+    context: string[];
+    projectId: string | null;
+}
+
+export interface Project extends WorkItem {
+    type: 'Project';
+    taskIds: string[];
+}
+
+export type AppEntries = Task | Project; 
+
+export interface AppData {
+    projects: Project[];
+    tasks: Task[];
+}
+
+export type InnerAction =
+    { type: 'ADD_PROJECT'; payload: {title: string} } |
+    { type: 'ADD_TASK'; payload: {title: string} } |
+    { type: 'UPDATE_TASK'; payload: { id: string; updates: Partial<Task> } } |
+    { type: 'UPDATE_PROJECT'; payload: { id: string; updates: Partial<Project> } } |
+    { type: 'DELETE_PROJECT'; payload: { id: string } } |
+    { type: 'DELETE_TASK'; payload: { id: string } };
+
+export type ProjectUpdateOption = Partial<Omit<Project, 'taskIds' | 'id' | 'createdAt'>>;
+export type TaskUpdateOption = Partial<Omit<Task, 'id' | 'createdAt'>>;
