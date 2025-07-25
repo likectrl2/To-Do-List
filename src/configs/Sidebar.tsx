288d01ba2f5a760a6f6db5425af3e642bf0b61eb
likectrl2 <likectrl2@gmail.com>
2025-07-25 18:35:07 +0800

    chore: initial update

--- src/configs/Sidebar.tsx
+++
@@ -1,8 +0,0 @@
-import { faFile, faHome } from "@fortawesome/free-solid-svg-icons";
-
-const SIDEBAR_CONFIG = [
-    { id: 0, icon: faHome, title: "Home" },
-    { id: 1, icon: faFile, title: "Manager" }
-]
-
-export default SIDEBAR_CONFIG
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
-import AppEntriesContextProvider from './contexts/AppEntriesContextProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <AppEntriesContextProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </AppEntriesContextProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
