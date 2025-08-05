ae23403a55a831e5087b41c6889720193b177555
likectrl2 <likectrl2@gmail.com>
2025-08-06 05:12:40 +0800

    feat: add text-shadow for wlecome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="page flex items-center justify-center">
-      <h1 className="text-neutral-000 shadow-blue-800 shadow">
+      <h1 className="text-white scale-150 [text-shadow:0_0_2px_#3399ff,0_0_5px_#3399ff,0_0_10px_#3399ff,0_0_25px_#3399ff,0_0_40px_#3399ff]">
         D.W.Y.L.
       </h1>
     </main>
