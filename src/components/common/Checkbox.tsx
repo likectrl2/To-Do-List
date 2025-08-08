f6a5a3bed9795b527e75eda252979f187532b4ad
likectrl2 <likectrl2@gmail.com>
2025-08-09 00:35:34 +0800

    feat: add style for checkbox

--- src/components/common/Checkbox.tsx
+++ src/components/common/Checkbox.tsx
@@ -7,12 +7,23 @@ interface CheckboxPara extends Omit<InputHTMLAttributes<HTMLInputElement>, "type
     className?: string;
 }
 
-export default function Checkbox({className, ...props}: CheckboxPara) {
+export default function Checkbox({className, checked, ...props}: CheckboxPara) {
     return (
-        <input
-            type="checkbox"
-            className={cn("aspect-square p-1", className)}
-            {...props}
-        />
+        <label className={cn("aspect-square transition relative rounded-sm", checked ? "bg-green-800 text-green-800" : "" , className)}>
+            <input
+                type="checkbox"
+                checked={checked}
+                className="h-0 w-0 absolute"
+                {...props}
+            />  
+            <svg viewBox="0 0 100 100" className="h-auto aspect-square rounded-sm absolute">
+                <rect width="100" height="100" fill="transparent" stroke="currentColor" strokeWidth="30" rx="30" ry="30"></rect>
+            </svg>
+            {
+                checked && <svg viewBox="0 0 100 100" className={cn("h-auto aspect-square rounded-sm absolute", checked ? "text-neutral-50" : "")}>
+                    <path d="M 20,50 L 40,75 L 80,25" stroke="currentColor" strokeWidth="15" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
+                </svg>
+            }
+        </label>
     )
 }
