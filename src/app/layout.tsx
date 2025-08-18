fbacb682e770a1ee64f8f3bb7c8724453dde72f5
likectrl2 <likectrl2@gmail.com>
2025-08-18 17:38:00 +0800

    feat: add a home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       lang='zh'
       className='h-full'
     >
-      <body className='h-full'>
-        <div className=''>{children}</div>
-      </body>
+      <body className='h-full overflow-hidden'>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 "use client";
 
+import { motion } from "framer-motion";
+
 export default function Home() {
   return (
-    <div className='page'>
-    </div>
+    <main className='page flex items-center justify-center'>
+      <motion.h1
+        className='text-3xl drop-shadow-[0_0_10px_#ffffff,0_0_40px_#ffffff]'
+        initial={{ opacity: 0, y: "100%" }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          opacity: { type: "tween", duration: 2, delay: 0.6 },
+          default: { type: "tween", duration: 2 },
+        }}
+      >
+        D.W.Y.L.
+      </motion.h1>
+    </main>
   );
 }
